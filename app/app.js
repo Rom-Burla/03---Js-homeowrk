7d71651782501d8535416ea0e000627a07a3389c
Rom Burla <romwebdev@gmail.com>
2022-06-29 22:23:28 +0300

    more efficient code

--- app/app.js
+++ app/app.js
@@ -7,23 +7,24 @@
 // null - false
 // undefined - false
 // blank string - false
-
+   
 
 for (var x = 1; x <= 100; x++) {
   if (x % 7 === 0 || x / 10 === 7 || x % 10 === 7) {
-    console.log(x + " Boom");
+    console.log(x+ " Boom");
     y++;
     if(y===2){
-      console.log(x + " double boom");
+      console.log(x + " Double boom");
     }
-  }else if(i=x.toString().includes('7')){console.log(x + ' Boom');
+  }else if(i=x.toString().includes('7')){
+    console.log(x + " Boom");
   i++;
    if(i===2){
-    console.log(x + ' double boom');
+    console.log(x + ' Double boom');
    }
 }else {
     console.log(x);
-    i = 0;
-    y = 0;
-  }
+   
+   y = 0;
+    }
 }
