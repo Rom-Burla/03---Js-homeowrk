18b54245a2ee3dbdf186ec6ca02ac28ecdd7c532
Rom Burla <romwebdev@gmail.com>
2022-06-29 21:31:22 +0300

    7 Boom!!! + Double boom!

---
+++ app/app.js
@@ -0,0 +1,30 @@
+// Assignment #1:
+
+// string - true
+// number - true
+// boolean - true/false as defined
+// NaN - false
+// null - false
+// undefined - false
+// blank string - false
+
+
+
+for (var x = 1; x <= 100; x++) {
+  if (x % 7 === 0 || x / 10 === 7 || x % 10 === 7) {
+    console.log(x + " Boom");
+    flag++;
+    if(flag===2){
+      console.log(x + " double boom");
+    }
+  }else if(y=x.toString().includes('7')){console.log(x + ' Boom');
+  y++;
+   if(y===2){
+    console.log(x + ' double boom');
+   }
+}else {
+    console.log(x);
+    flag = 0;
+    y = 0;
+  }
+}
